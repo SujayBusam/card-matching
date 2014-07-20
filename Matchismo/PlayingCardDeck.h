52c9b324c555ee27c25971a7e1733d603888bed0
Sujay Busam <sujay.busam@gmail.com>
2014-07-19 21:49:47 -0400

    PlayingCardDeck model complete. View started.

---
+++ Matchismo/PlayingCardDeck.h
@@ -0,0 +1,15 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo
+//
+//  Created by Sujay Busam on 7/19/14.
+//  Copyright (c) 2014 Sujay's Dev Center. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+// Inherit all we need from Deck
+
+@end
---
+++ Matchismo/PlayingCardDeck.m
@@ -0,0 +1,33 @@
+//
+//  PlayingCardDeck.m
+//  Matchismo
+//
+//  Created by Sujay Busam on 7/19/14.
+//  Copyright (c) 2014 Sujay's Dev Center. All rights reserved.
+//
+
+#import "PlayingCardDeck.h"
+#import "PlayingCard.h"
+
+@implementation PlayingCardDeck
+
+- (instancetype)init
+{
+    self = [super init];
+    
+    if (self) {
+        // Iterate through all ranks and suits
+        for (NSString *suit in [PlayingCard validSuits]) {
+            for (NSUInteger rank = 1; rank <= [PlayingCard maxRank]; rank++) {
+                PlayingCard *card = [[PlayingCard alloc] init];
+                card.suit = suit;
+                card.rank = rank;
+                [self addCard:card];
+            }
+        }
+    }
+    
+    return self;
+}
+
+@end
